c3f03eeaf8e1b47688e669b3c606102407112aae
Eric Gross <eric@buds.com>
2015-04-14 08:15:53 -0400

    Allow editing of products via web interface from /products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,6 +8,22 @@ class ProductsController < ApplicationController
     @product = Product.new
   end
 
+  def index
+    @products = Product.all.sort_by(&:name)
+  end
+
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    @product.update_attributes(product_params)
+    render action: :edit unless @product.valid?
+
+    redirect_to action: :index
+  end
+
   private
 
   def product_params
